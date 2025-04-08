cca808285ae076e58b8f374e6fa396a6db4cd237
mnak94 <na24adx@herts.ac.uk>
2025-04-08 23:56:11 +0500

    Create Physiotherapist.java

---
+++ Physiotherapist.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Physiotherapist {
+    private int id;
+    private String name;
+    private String address;
+    private String phone;
+    private String[] specialties;
+    private List<Treatment> treatmentTimetable;
+    private static int idCounter = 1; // Static counter for auto-increment
+
+
+    // Constructor
+    public Physiotherapist(String name, String address, String phone, String[] specialties, List<Treatment> treatmentTimetable) {
+        this.id = idCounter++; 
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.specialties = specialties;
+        this.treatmentTimetable = treatmentTimetable;
+    }
+}
