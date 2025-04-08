45aca815e8d926c4118c8a4a832d712bcf52e69a
mnak94 <na24adx@herts.ac.uk>
2025-04-08 23:58:13 +0500

    Create Functionalities.java

---
+++ Functionalities.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Functionalities {
+    public List<Patient> patients = new ArrayList<>();
+    public List<Physiotherapist> therapists = new ArrayList<>();
+    public List<Booking> bookings = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+}
