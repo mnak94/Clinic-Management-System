b859be957f5b8c7d693a06662b306b98ef5216b7
mnak94 <na24adx@herts.ac.uk>
2025-04-10 00:33:31 +0500

    Update Functionalities.java
    Added addPatient functionality

--- Functionalities.java
+++ Functionalities.java
@@ -5,4 +5,16 @@ public class Functionalities {
     public List<Physiotherapist> therapists = new ArrayList<>();
     public List<Booking> bookings = new ArrayList<>();
     private Scanner scanner = new Scanner(System.in);
+
+    public void addPatient() {
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter address: ");
+        String addr = scanner.nextLine();
+        System.out.print("Enter phone: ");
+        String phone = scanner.nextLine();
+        patients.add(new Patient(name, addr, phone));
+        System.out.println("Patient added.");
+    }
+
 }
