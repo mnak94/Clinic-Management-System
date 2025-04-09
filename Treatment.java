206342c0928ffb0fbed9f6661045f9b6ee9cb2a7
mnak94 <na24adx@herts.ac.uk>
2025-04-10 00:31:13 +0500

    Update Treatment.java
    Further developed treatment class

--- Treatment.java
+++ Treatment.java
@@ -10,4 +10,45 @@ public class Treatment {
         this.dateTime = dateTime;
         this.physiotherapist = physiotherapist;
     }
+    
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void book() {
+        isBooked = true;
+    }
+
+    public void cancel() {
+        isBooked = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Physiotherapist getPhysiotherapist() {
+        return physiotherapist;
+    }
+
+    public void setPhysiotherapist(Physiotherapist physiotherapist) {
+        this.physiotherapist = physiotherapist;
+    }
+
+    @Override
+    public String toString() {
+        return name + " at " + dateTime + (isBooked ? " (Booked)" : " (Available)");
+    }
 }
