93e801cdb9fcc41e64cc4f199526920154d16b38
mnak94 <na24adx@herts.ac.uk>
2025-04-10 00:29:00 +0500

    Update Patient.java
    Further development of patients

--- Patient.java
+++ Patient.java
@@ -12,4 +12,21 @@ public class Patient {
         this.address = address;
         this.phone = phone;
     }
+    public int getId() { return id; }
+    public String getAddress() { return address; }
+    public String getName() { return name; }
+    public String getPhone() { return phone; }
+
+    public static int getIdCounter() {
+        return idCounter;
+    }
+
+    public static void setIdCounter(int idCounter) {
+        Patient.idCounter = idCounter;
+    }
+    
+    @Override
+    public String toString() {
+        return "Patient #" + id + ": " + name + ", " + address + ", " + phone;
+    }
 }
