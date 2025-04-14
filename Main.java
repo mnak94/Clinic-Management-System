0d2c9a1b9121f5b5349e9e19824052628bc56881
mnak94 <na24adx@herts.ac.uk>
2025-04-14 17:12:21 +0500

    Update Main.java
    Unlocked the bookappointment and listing functionalities

--- Main.java
+++ Main.java
@@ -29,13 +29,13 @@ public class Main {
             switch (choice) {
                 case 1 -> f.addPatient();
                 case 2 -> f.removePatient();
-                // case 3 -> f.bookAppointment();
+                case 3 -> f.bookAppointment();
                 // case 4 -> f.cancelAppointment();
                 // case 5 -> f.changeAppointment();
                 // case 6 -> f.attendAppointment();
-                // case 7 -> f.listPatients();
-                // case 8 -> f.listPhysiotherapists();
-                // case 9 -> f.listBookings();
+                case 7 -> f.listPatients();
+                case 8 -> f.listPhysiotherapists();
+                case 9 -> f.listBookings();
                 // case 10 -> f.printReport();
                 case 0 -> System.out.println("Goodbye!");
                 default -> System.out.println("Invalid choice!");
